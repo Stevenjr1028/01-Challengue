5c10a1663a3dfe86f13380f289bfe045fe780dd8
Gerson Steven <98818664+Stevenjr1028@users.noreply.github.com>
2023-03-02 14:52:43 -0500

    Challenge04

--- FirstApp/src/main.jsx
+++ FirstApp/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App'
 import './index.css'
-import FirstApp from './FirstApp'
+//import FirstApp from './FirstApp'
+import { GifExpertApp } from './ComponentApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-    <FirstApp value={1}/>
+    <GifExpertApp/>
 
   </React.StrictMode>,
 )
